23c10e641993838d49565463867901f65a31a2b3
Musharraf74 <mohammadmusharraf97@gmail.com>
2026-03-08 03:22:58 +0530

    Fix formatting issue in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
   /* =========================
      1) Grab page elements
   ========================== */
